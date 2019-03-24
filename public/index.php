a68ab5dc8329cfc343ddc5e1b5adafac3a0f3540
li <ls447@njit.edu>
2019-03-24 18:31:23 -0400

    Feature: Reading csv File

---
+++ public/index.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: li
+ * Date: 3/24/19
+ * Time: 5:07 PM
+ */
+main::start("example.csv");
+
+
+class main {
+    static public function start($filename) {
+
+        $records = csv::getRecords($filename);
+
+        $table =html::generateTable($records);
+    }
+}
+
+class html {
+
+    public static function generateTable($records) {
+
+        echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+                crossorigin="anonymous">';
+
+        echo '<head><tilte>tfboys |_(^_^)_|</tilte></head>';
+
+        echo'<table class="table table-striped"><thead><tr>';
+
+        $count = 0;
+
+        print("<table border='1'>");
+        foreach ($records as $record) {
+
+            if ($count == 0) {
+                print("<tr class = 'header'>");
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                foreach ($fields as $columnName)
+                {
+                    print("<th>");
+                    print_r($columnName);
+                    print("</th>");
+                }
+                print("</tr>");
+            }
+
+            {
+                print("<tr>");
+                $array = $record->returnArray();
+                $values = array_values($array);
+                foreach ($values as $columnValue)
+                {
+                    print("<td>");
+                    print_r($columnValue);
+                    print("</td>");
+                }
+                print("</tr>");
+            }
+            $count++;
+        }
+        print("</table>");
+        print("</html>");
+    }
+}
+
+class csv {
+
+    static public function getRecords($filename) {
+
+
+        $file = fopen($filename,"r");
+
+        $fieldNames = array();
+
+        $count = 0;
+
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+
+            if($count == 0) {
+
+                $fieldNames = $record;
+            } else {
+
+                $records[] = recordFactory::create($fieldNames, $record);
+            }
+            $count++;
+        }
+        fclose($file);
+        return $records;
+    }
+}
+
+class record {
+    public function __construct(Array $fieldNames = null, $values = null)  {
+
+        $record = array_combine($fieldNames, $values);
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+        }
+    }
+
+    public function returnArray() {
+        $array = (array) $this;
+        return $array;
+
+    }
+    public function createProperty($name = 'first', $value ='jackson') {
+        $this->{$name} = $value;
+    }
+}
+
+
+class recordFactory {
+    public static function create(Array $fieldNames = null, Array $values = null) {
+
+        $record = new record($fieldNames, $values);
+        return $record;
+    }
+}
